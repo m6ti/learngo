a9bcde0e9c0618f548888f815ba79f3f4207379e
Mati K <matijklocek@gmail.com>
2025-06-09 16:34:54 +0100

    Add a test

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -12,6 +12,15 @@ func TestHelloName(t *testing.T) {
 	want := regexp.MustCompile(`\b` + name + `\b`)
 	msg, err := Hello("Gladys")
 	if !want.MatchString(msg) || err != nil {
-		t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err)
+		t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+// TestHelloEmpty calls greeting.Hello with an empty string,
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Errorf(`Hello("") = %q, %v, want "", error`, msg, err)
 	}
 }
