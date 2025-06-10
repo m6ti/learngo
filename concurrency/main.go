0c301643cebe513227719c54f5492b7f43a5996d
Mati K <matijklocek@gmail.com>
2025-06-10 09:59:59 +0100

    Channels

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
+	c := make(chan string)
+	go count("sheep", c)
 
-	go func() {
-		count("sheep")
-		wg.Done()
-	}()
-
-	wg.Wait()
+	for msg := range c {
+		fmt.Println(msg)
+	}
 }
 
-func count(thing string) {
+func count(thing string, c chan string) {
 	for i := 1; i <= 5; i++ {
-		fmt.Println(i, thing)
+		c <- thing
 		time.Sleep(time.Millisecond * 500)
 	}
+
+	close(c)
 }
